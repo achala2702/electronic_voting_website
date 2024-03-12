8828d7a658e0d9c4e72cf3af28618c3b5cc40aab
Achala Nipun <achala2702@gmail.com>
2024-03-12 11:21:10 +0530

    bar chart

--- frontend/src/components/barGraph/BarChart.js
+++ frontend/src/components/barGraph/BarChart.js
@@ -1,9 +1,47 @@
 import React from 'react'
 import "./barChart.css"
+import {Chart as ChartJS,
+BarElement,
+CategoryScale,
+LinearScale,
+Tooltip,
+Legend} from "chart.js"
+import {Bar} from "react-chartjs-2"
+
+ChartJS.register(
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+)
 
 const BarChart = () => {
+
+  const names = ["first", "second", "third"]
+  const votes = [100, 300, 400]
+
+  const data = {
+    labels: names,
+    datasets: [{
+      label: "Vote Count",
+      data: votes,
+      backgroundColor: "aqua",
+      borderColor: "black",
+      borderWidth: 1,
+    }]
+  }
+
+  const options = {
+    responsive: true, // makes chart resposive
+    maintainAspectRatio: false //turn off the automatic aspect ratio
+  }
+
   return (
-    <div>BarChart</div>
+    <div id="bar-chart">
+      <Bar data = {data}
+      options = {options}/>
+    </div>
   )
 }
 
--- frontend/src/containers/webBody/Body.js
+++ frontend/src/containers/webBody/Body.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./body.css"
 import BarChart from '../../components/barGraph/BarChart'
-import PieChart from '../../components/barGraph/BarChart'
+import PieChart from '../../components/pieChart/PieChart'
 import CandidateCon from '../../components/candidateCon/CandidateCon'
 
 const Body = () => {
