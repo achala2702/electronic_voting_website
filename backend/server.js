c8c761f279ca1446b28ab4beb76163c9ba76a065
Achala Nipun <achala2702@gmail.com>
2024-03-13 18:02:48 +0530

    connected the backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const {Server} = require("socket.io")
+const cors = require("cors")
 const port = 5000;
+
+app.use(cors());
+
+//creating the server
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
 
 //getting candidate names
 const candidateNames = ["name1", "name2", "name3", "name4"];
@@ -23,6 +37,11 @@ app.get('/', (req, res)=>{
     res.json(candidates);
 });
 
-app.listen(port, ()=>{
+//sending data to the frontend
+io.on("connection", (socket)=>{
+    console.log(`Client Connected: ${socket.id}`)
+})
+
+server.listen(port, ()=>{
     console.log(`server started on port ${port}`);
 })
--- frontend/src/containers/webBody/Body.js
+++ frontend/src/containers/webBody/Body.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import "./body.css"
-import BarChart from '../../components/barGraph/BarChart'
-import PieChart from '../../components/pieChart/PieChart'
-import CandidateCon from '../../components/candidateCon/CandidateCon'
+import React from 'react';
+import "./body.css";
+import BarChart from '../../components/barGraph/BarChart';
+import PieChart from '../../components/pieChart/PieChart';
+import CandidateCon from '../../components/candidateCon/CandidateCon';
+import io from "socket.io-client";
+
+//connecting to the server
+const socket = io("http://localhost:5000");
+
+socket.on("connect", () => {
+  console.log(`conected to the server ${socket.connected}`);
+});
 
 const Body = () => {
   return (
