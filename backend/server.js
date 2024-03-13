e91622644ce1034217a3c5d84c59dddba84272e3
Achala Nipun <achala2702@gmail.com>
2024-03-13 12:41:02 +0530

    map candidates to an array and send check

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+
+//getting candidate names
+const candidateNames = ["name1", "name2", "name3", "name4"];
+
+//getting their voteCount
+const voteCounts = [110, 200, 400, 50]
+
+//mapping the candidateNames and voteCounts into an array of obejcts
+const candidates = candidateNames.map((name, index)=>({
+    name,
+    voteCount: voteCounts[index]
+}));
+
+//sorting the candidates array in decending order according to the voteCount
+candidates.sort((a, b)=> b.voteCount - a.voteCount)
+
 
 //sending a respond to check whether the backend works or not
 app.get('/', (req, res)=>{
-    res.json("hello from the backend!");
+    res.json(candidates);
 });
 
 app.listen(port, ()=>{
